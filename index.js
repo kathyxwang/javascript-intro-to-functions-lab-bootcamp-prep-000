43711950c764e7a9136b06449e6d7507ecc3892d
Kathy Wang <kathy.x.wang@gmail.com>
2018-08-18 01:56:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,6 +19,6 @@ function sayHiToGrandma(string) {
     return "I love you too."
   } else if string.toUpperCase === string {
     return "YES INDEED!" 
-  } else return "I"
+  } else return "I can't hear you!"
   }
 }
